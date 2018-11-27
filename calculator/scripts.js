08a345e8c87dad7c83132181aff6b02d3f99ca3a
joy <wendokingoo@gmail.com>
2018-11-27 08:15:17 +0300

    jscript and html file added

---
+++ calculator/scripts.js
@@ -0,0 +1,13 @@
+var getAdd=function(numb1,numb2){
+return (numb1+numb2);
+};
+var numb1=parseInt(prompt("Enter your first number:"));
+var numb2=parseInt(prompt("Second number is:"));
+alert(getAdd(numb1,numb2));
+
+var name =function(getName){
+
+}
+var name =prompt("username");
+
+alert("Your name is " +name +" Nice to meet you :/");
